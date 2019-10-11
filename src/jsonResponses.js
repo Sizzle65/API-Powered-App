6ac4f02cca24bf0ab6cb644c0126c79b91653aa6
Sam Belisle <spb6892@rit.edu>
2019-10-11 12:41:51 -0400

    Finished project

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,3 +1,4 @@
+// Characters object to hold all the character information
 const characters = {};
 
 const respondJSON = (request, response, status, object) => {
@@ -13,7 +14,21 @@ const respondJSONMeta = (request, response, status) => {
   response.end();
 };
 
+const deactiveAllItems = (name) => {
+  for (let index = 0; index < characters[name].inventory.length; index++) {
+    characters[name].inventory[index].active = false;
+  }
+};
+
+// Grabs the selected character or creates them if they don't exist
 const getCharacter = (request, response, params) => {
+  // Deactivate all items for all characters to handle page refreshes,
+  // ensuring all items get added to UI properly
+  const names = Object.keys(characters);
+  for (let index = 0; index < names.length; index++) {
+    deactiveAllItems(names[index]);
+  }
+
   const { name } = params;
   let responseJSON = {};
 
@@ -26,11 +41,15 @@ const getCharacter = (request, response, params) => {
       inventory: [],
     };
   }
-
   responseJSON = characters[name];
   return respondJSON(request, response, responseCode, responseJSON);
 };
 
+const getCharacterMeta = (request, response) => {
+  respondJSONMeta(request, response, 200);
+};
+
+// Add item to the characters inventory array
 const addItem = (request, response, params) => {
   let responseJSON = {
     message: 'Name, item, quantity, weight and price are required.',
@@ -42,8 +61,10 @@ const addItem = (request, response, params) => {
   } else if (characters[params.name]) {
     const count = characters[params.name].inventory.length;
 
+    // Creates a random item id for each item
     const randomId = Math.floor(Math.random() * 100000 + 1);
 
+    // Sets the passed in values for the item and adds it to the inventory
     characters[params.name].inventory[count] = {
       item: params.item,
       quantity: params.quantity,
@@ -63,6 +84,7 @@ const addItem = (request, response, params) => {
   return respondJSON(request, response, statusCode, responseJSON);
 };
 
+// Sets the specific items active property to true to ensure it is added to the inventory UI
 const activateItem = (request, response, params) => {
   const responseJSON = {
     message: 'Name and item are required.',
@@ -89,6 +111,24 @@ const activateItem = (request, response, params) => {
   return respondJSON(request, response, statusCode, responseJSON);
 };
 
+// Sets the items active property to false to ensure they aren't added to the UI
+const deactivateItem = (request, response, params) => {
+  const responseJSON = {
+    message: 'Character does not exist.',
+  };
+  let statusCode = 400;
+
+  if (characters[params.name]) {
+    deactiveAllItems(params.name);
+
+    responseJSON.message = 'Character item deactivation successful.';
+    statusCode = 200;
+  }
+
+  return respondJSON(request, response, statusCode, responseJSON);
+};
+
+// Deletes selected item from the characters inventory array
 const deleteItem = (request, response, params) => {
   const responseJSON = {
     message: 'Name and id are required.',
@@ -98,13 +138,34 @@ const deleteItem = (request, response, params) => {
   if (!params.name || !params.id) {
     responseJSON.id = 'missingParams';
   } else if (characters[params.name]) {
-    const { inventory } = characters[params.name];
-
-    for (let x = 0; x < inventory.length; x++) {
-      if (inventory[x].id === params.id) {
-        inventory.splice(x);
+    for (let x = 0; x < characters[params.name].inventory.length; x++) {
+      // Finds the specific item per the item id and splices it from the array
+      if (characters[params.name].inventory[x].id.toString() === params.id) {
+        characters[params.name].inventory.splice(x, 1);
       }
     }
+
+    return respondJSONMeta(request, response, 201);
+  } else {
+    responseJSON.message = 'Character not found.';
+    responseJSON.id = 'notFound';
+    statusCode = 404;
+  }
+  return respondJSON(request, response, statusCode, responseJSON);
+};
+
+// Clears current inventory for selected character
+const clearInventory = (request, response, params) => {
+  const responseJSON = {
+    message: 'Name is required.',
+  };
+  let statusCode = 400;
+
+  if (!params.name) {
+    responseJSON.id = 'missingParams';
+  } else if (characters[params.name]) {
+    // Empties the characters inventory array
+    characters[params.name].inventory = [];
     return respondJSONMeta(request, response, 201);
   } else {
     responseJSON.message = 'Character not found.';
@@ -115,9 +176,40 @@ const deleteItem = (request, response, params) => {
   return respondJSON(request, response, statusCode, responseJSON);
 };
 
+// Deletes selected character from the characters object
+const deleteCharacter = (request, response, params) => {
+  const responseJSON = {
+    message: 'Name is required.',
+  };
+  let statusCode = 400;
+
+  if (!params.name) {
+    responseJSON.id = 'missingParams';
+  } else if (characters[params.name]) {
+    // Deletes the character from the characters object
+    delete characters[params.name];
+    return respondJSONMeta(request, response, 201);
+  } else {
+    responseJSON.message = 'Character not found.';
+    responseJSON.id = 'notFound';
+    statusCode = 404;
+  }
+
+  return respondJSON(request, response, statusCode, responseJSON);
+};
+
+const notRealMeta = (request, response) => {
+  respondJSONMeta(request, response, 404);
+};
+
 module.exports = {
   getCharacter,
   addItem,
   activateItem,
+  deactivateItem,
   deleteItem,
+  clearInventory,
+  deleteCharacter,
+  getCharacterMeta,
+  notRealMeta,
 };
--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-// handle POST requests
+// Handle POST requests
 const handlePost = (request, response, parsedUrl) => {
   if (parsedUrl.pathname === '/addItem') {
     const params = query.parse(parsedUrl.query);
@@ -16,7 +16,7 @@ const handlePost = (request, response, parsedUrl) => {
   }
 };
 
-// handle GET requests
+// Handle GET requests
 const handleGet = (request, response, parsedUrl) => {
   if (parsedUrl.pathname === '/style.css') {
     htmlHandler.getCSS(request, response);
@@ -25,44 +25,54 @@ const handleGet = (request, response, parsedUrl) => {
   } else if (parsedUrl.pathname === '/getCharacter') {
     const params = query.parse(parsedUrl.query);
     jsonHandler.getCharacter(request, response, params);
-  } else if (parsedUrl.pathname === '/addItem') {
-    const params = query.parse(parsedUrl.query);
-    jsonHandler.addItem(request, response, params);
-  } else if (parsedUrl.pathname === '/activateItem') {
-    const params = query.parse(parsedUrl.query);
-    jsonHandler.activateItem(request, response, params);
-  } else if (parsedUrl.pathname === '/deleteItem') {
-    const params = query.parse(parsedUrl.query);
-    jsonHandler.deleteItem(request, response, params);
   } else if (parsedUrl.pathname === '/') {
     htmlHandler.getIndex(request, response);
   }
 };
 
-// const handleHead = (request, response, parsedUrl) => {
+// Handle HEAD requests
+const handleHead = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/getCharacter') {
+    jsonHandler.getCharacterMeta(request, response);
+  } else {
+    jsonHandler.notRealMeta(request, response);
+  }
+};
 
-// };
-
+// Handle PUT requests
 const handlePut = (request, response, parsedUrl) => {
   if (parsedUrl.pathname === '/activateItem') {
     const params = query.parse(parsedUrl.query);
     jsonHandler.activateItem(request, response, params);
+  } else if (parsedUrl.pathname === '/deactivateItem') {
+    const params = query.parse(parsedUrl.query);
+    jsonHandler.deactivateItem(request, response, params);
   }
 };
 
+// Handle DELETE requests
 const handleDelete = (request, response, parsedUrl) => {
   if (parsedUrl.pathname === '/deleteItem') {
     const params = query.parse(parsedUrl.query);
     jsonHandler.deleteItem(request, response, params);
+  } else if (parsedUrl.pathname === '/clearInventory') {
+    const params = query.parse(parsedUrl.query);
+    jsonHandler.clearInventory(request, response, params);
+  } else if (parsedUrl.pathname === '/deleteCharacter') {
+    const params = query.parse(parsedUrl.query);
+    jsonHandler.deleteCharacter(request, response, params);
   }
 };
 
+// Recieves the request and calls the appropriate method
 const onRequest = (request, response) => {
   console.log(request.url);
   const parsedUrl = url.parse(request.url);
 
   if (request.method === 'GET') {
     handleGet(request, response, parsedUrl);
+  } else if (request.method === 'HEAD') {
+    handleHead(request, response, parsedUrl);
   } else if (request.method === 'POST') {
     handlePost(request, response, parsedUrl);
   } else if (request.method === 'PUT') {
